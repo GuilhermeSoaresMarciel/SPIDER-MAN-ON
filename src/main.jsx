3abfefeb64498964da80eae680a807a8589d12c2
Guilherme Soares <guilhermesoaresmarciel.dev@gmail.com>
2026-08-30 22:16:46 -0300

    ON

---
+++ src/main.jsx
@@ -0,0 +1,53 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+const bg_v1 = "bg-zinc-900";
+const bg_v2 = "bg-zinc-800";
+const bg_v3 = "bg-zinc-700";
+
+const text_color_v1 = "text-white";
+const text_color_v2 = "text-sky-500";
+
+const font_main = "font-sans";
+
+const p = "p-2";
+const gap = "gap-2";
+
+createRoot(document.querySelector("body")).render(
+  <StrictMode>
+    <main className={`min-h-[100dvh] ${bg_v1} ${text_color_v1} ${font_main}`}>
+      <header className={`${bg_v2} flex justify-between items-center ${p}`}>
+        <img className="w-[100px]" src="/logo.png" />
+        <nav className={`flex ${gap}`}>
+          <a href="#Inicio">Início</a>
+          <a href="#Sobre">Sobre</a>
+          <a href="#Elenco">Elenco</a>
+        </nav>
+      </header>
+      <section
+        className={`h-[100vh] flex flex-col justify-center items-center ${gap} font-extrabold bg-[url(./assets/hero-bg.jpg)] bg-center bg-no-repeat bg-cover`}
+        id="Inicio"
+      >
+        <h1 className={`text-4xl ${text_color_v2}`}>SEM VOLTA PARA CASA</h1>
+        <h1 className="text-3xl">
+          A lenda dos cinemas tem nome
+          <span className={`text-4xl ${text_color_v2}`}>?</span>
+        </h1>
+        <button
+          className={`${bg_v2} rounded-2xl ${p} transition-all hover:scale-110 hover:${bg_v3}`}
+        >
+          Explorar Agora!
+        </button>
+      </section>
+      <footer>
+        <h6 className={`${bg_v2} ${p} text-center`}>
+          &copy; 2026 Criado por um fã do Aranha.
+          <br />
+          Todos os direitos reservados à Marvel & Sony.
+        </h6>
+      </footer>
+    </main>
+  </StrictMode>,
+);
